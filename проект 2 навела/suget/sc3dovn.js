a5a4acf216e304001b005295c277661505f67bcd
dangstu <127624758+dangstu@users.noreply.github.com>
2023-04-11 17:31:57 +0300

    Add files via upload

---
+++ проект 2 навела/suget/sc3dovn.js
@@ -0,0 +1,11 @@
+// var w = window.screen.availWidth;
+// if(w > 1000){
+// 	let text = document.querySelector('.text')
+// 	let str = document.querySelector('#str')
+// 	window.addEventListener('mousemove',function(e){
+// 		let x = e.clientX / window.innerWidth;
+// 		let y = e.clientY / window.innerHeight;
+// 		text.style.transform = 'translate(+' + x * 25 + 'px, +' + y * 25 + 'px)';
+// 		str.style.transform = 'translate(-' + x * 10 + 'px, -' + y * 10 + 'px)';
+// 	});
+// };
